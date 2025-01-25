e994d4928ec9b39c578595ecf8cb8b881c85781e
guilou <guilhemclr@gmail.com>
2025-01-25 14:23:23 +0100

    initial commit

---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'sakila',
+      autoLoadEntities: true,
+      synchronize: false,
+    }),
+  ],
+})
+export class AppModule {}
