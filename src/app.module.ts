6bd37709a57e1c282320affa4f6f03f96421354a
guilou <guilhemclr@gmail.com>
2025-01-25 14:50:05 +0100

    db succesfully connected

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { CustomerModule } from './customer/customer.module';
 
 @Module({
   imports: [
@@ -13,6 +14,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       autoLoadEntities: true,
       synchronize: false,
     }),
+    CustomerModule,
   ],
 })
 export class AppModule {}
